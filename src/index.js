412960fda2e81b1d031a8dfb631a31c325c5c241
Marcos Valderrabano <ing.marco.valu@gmail.com>
2022-07-05 19:45:38 -0700

    Completed lecture

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
-function Home() {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-}
+/* add the NavBar component to our render method */
 
-function App() {
-  return <Home />;
-}
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+  <App/>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
